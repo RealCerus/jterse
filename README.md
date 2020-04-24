90950da64529f5668df58bf8ec3c3a4d825f78c1
Cerus <zocks1703@web.de>
2020-04-24 23:07:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# jterse
+A [Terse](https://esolangs.org/wiki/Terse) interpreter written in Java.
+
+## Note
+This is a work in progress
---
+++ src/main/java/de/cerus/jterse/JTerse.java
@@ -0,0 +1,14 @@
+package de.cerus.jterse;
+
+public class JTerse {
+
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            // Parse file arguments n stuff
+            return;
+        }
+
+        // Start interpreter in REPL mode
+    }
+
+}
